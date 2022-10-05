fa7e1163db86e47fa035f417b2d36c9b0c0e7593
Radzeer <hollo.attila.82@gmail.com>
2022-10-05 10:06:26 +0200

    jsf table

---
+++ src/main/java/training/employees/employees/jsf/EmployeesBackingBean.java
@@ -0,0 +1,29 @@
+package training.employees.employees.jsf;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+import training.employees.employees.dto.EmployeeDto;
+import training.employees.employees.service.EmployeesService;
+
+import javax.annotation.PostConstruct;
+import java.awt.dnd.DragSourceMotionListener;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequestScope
+@AllArgsConstructor
+public class EmployeesBackingBean {
+
+    private EmployeesService service;
+
+    @Getter
+    private List<EmployeeDto> employees;
+
+    @PostConstruct
+    public void initEmployees(){
+        employees = service.listEmployees(Optional.empty());
+    }
+}
