46709d725ea38f71d37418f1e6414c6b694f3b8a
Radzeer <hollo.attila.82@gmail.com>
2022-10-05 10:33:37 +0200

    jsf form, command, create employee

--- src/main/java/training/employees/employees/jsf/EmployeesBackingBean.java
+++ src/main/java/training/employees/employees/jsf/EmployeesBackingBean.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
+import training.employees.employees.dto.CreateEmployeeCommand;
 import training.employees.employees.dto.EmployeeDto;
 import training.employees.employees.service.EmployeesService;
 
@@ -20,10 +21,17 @@ public class EmployeesBackingBean {
     private EmployeesService service;
 
     @Getter
+    private final CreateEmployeeCommand command = new CreateEmployeeCommand();
+    @Getter
     private List<EmployeeDto> employees;
 
     @PostConstruct
     public void initEmployees(){
         employees = service.listEmployees(Optional.empty());
+    }
+
+    public String createEmployee(){
+        service.createEmployee(command);
+        return "employees.xhtml?faces-redirect=true";
     }
 }
